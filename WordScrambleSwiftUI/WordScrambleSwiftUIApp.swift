58b66d253bcdf7572e20e13da4fc1ba3ed0e4b55
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-08-17 18:54:24 +0300

    Initial Commit

---
+++ WordScrambleSwiftUI/WordScrambleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleSwiftUIApp.swift
+//  WordScrambleSwiftUI
+//
+//  Created by Николай Никитин on 17.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
