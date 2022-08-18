408f45346d24888d8f747320dc66cde16033a281
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-08-19 00:38:14 +0300

    Running code when app launches

--- WordScrambleSwiftUI/ContentView.swift
+++ WordScrambleSwiftUI/ContentView.swift
@@ -8,10 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+
+  //MARK: - Properties
+  @State private var usedWords = [String]()
+  @State private var rootWord = ""
+  @State private var newWord = ""
+
+  //MARK: - View
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+      NavigationView {
+        List {
+          Section {
+            TextField("Enter your word", text: $newWord)
+              .textInputAutocapitalization(.none)
+          }
+
+          Section {
+            ForEach(usedWords, id: \.self) { word in
+              HStack {
+                Image(systemName: "\(word.count).circle")
+                Text(word)
+              }
+            }
+          }
+        }
+        .navigationTitle(rootWord)
+        .onSubmit(addNewWord)
+        .onAppear(perform: startGame)
+      }
     }
+
+  //MARK: - Methods
+  func addNewWord() {
+    let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+    guard answer.count > 0 else { return }
+    withAnimation {
+      usedWords.insert(answer, at: 0)
+    }
+    newWord = ""
+  }
+
+  func startGame() {
+    if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+      if let startWords =  try? String(contentsOf: startWordsURL) {
+        let allWords = startWords.components(separatedBy: "\n")
+        rootWord = allWords.randomElement() ?? "silkworm"
+        return
+      }
+    }
+    fatalError("Couldn't load start.txt from bundle!")
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
